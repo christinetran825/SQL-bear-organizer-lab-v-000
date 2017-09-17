059e1276a35879e2636b6720a98a13f97e982195
Christine Tran <christinetran825@gmail.com>
2017-09-17 23:07:13 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -14,7 +14,7 @@ end
 
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "SELECT name FROM bears ORDER BY ABC;"
+  "SELECT name FROM bears ORDER BY ASC;"
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
